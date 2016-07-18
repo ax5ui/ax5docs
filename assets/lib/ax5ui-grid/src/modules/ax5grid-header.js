7d3ce81b70e1ae15360d227e30925031d2b1e480
Thomas Jang <tom@axisj.com>
2016-07-18 22:05:24 +0900

    07/18

---
+++ assets/lib/ax5ui-grid/src/modules/ax5grid-header.js
@@ -0,0 +1,21 @@
+
+/*
+ * Copyright (c) 2016. tom@axisj.com
+ * - github.com/thomasjang
+ * - www.axisj.com
+ */
+
+// ax5.ui.grid.header
+(function (root) {
+    "use strict";
+
+    
+    var init = function(){
+        console.log(this.columns);
+    };
+
+    root.header = {
+        init: init
+    };
+
+})(ax5.ui.grid);
---
+++ assets/lib/ax5ui-grid/src/modules/ax5grid-scroller.js
@@ -0,0 +1,21 @@
+
+/*
+ * Copyright (c) 2016. tom@axisj.com
+ * - github.com/thomasjang
+ * - www.axisj.com
+ */
+
+// ax5.ui.grid.scroller
+(function (root) {
+    "use strict";
+
+    
+    var init = function(){
+
+    };
+
+    root.scroller = {
+        init: init
+    };
+
+})(ax5.ui.grid);
